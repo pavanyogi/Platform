ea442639e0ed78514747ca3517ad340722cfc681
Pavan Yogi <pavanyogi121@gmail.com>
2022-02-21 15:44:58 +0530

    Resolve routes issue for telegram login functionality

--- platform/plugins/Users/handlers/Users/webhook/post.php
+++ platform/plugins/Users/handlers/Users/webhook/post.php
@@ -1,2 +1,8 @@
 <?php
 
+function Users_webhook_post()
+{
+    // just for supporting the method, so we can send large batch requests
+    // in the body of the POST
+    return 'Users_webhook_post';
+}
---
+++ platform/plugins/Users/handlers/Users/webhook/response/content.php
@@ -0,0 +1,8 @@
+<?php
+
+function Users_webhook_response_content()
+{
+//    Q.req("Users/webhook", function () {});
+//    Q.req("Users/webhook", ["slot1", "slot2", ...], function () {}, {method: "post", fields: {...}});
+    return 'Users_webhook_response_content';
+}
