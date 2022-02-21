0b780fe573a372a5b705203c117c3eb59fcd863c
Pavan Yogi <pavanyogi121@gmail.com>
2022-02-21 19:46:47 +0530

    Add setWebhook call inside Users/webhook post method

--- platform/plugins/Users/handlers/Users/webhook/post.php
+++ platform/plugins/Users/handlers/Users/webhook/post.php
@@ -1,8 +1,13 @@
 <?php
 
-function Users_webhook_post()
+function Users_webhook_post($params)
 {
     // just for supporting the method, so we can send large batch requests
     // in the body of the POST
-    return 'Users_webhook_post';
+//    return 'Users_webhook_post';
+
+    $req = array_merge($_REQUEST, $params);
+
+    Users_ExternalFrom_Telegram::setWebhook();
+
 }
