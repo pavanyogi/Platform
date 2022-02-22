0840b2b21e6b57716e9c1fe9eefc1d56922bef95
Pavan Yogi <pavanyogi121@gmail.com>
2022-02-22 13:32:40 +0530

    Add /start command

---
+++ platform/plugins/Users/classes/Users/ExternalFrom/Commands/StartCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of the PHP Telegram Bot example-bot package.
+ * https://github.com/php-telegram-bot/example-bot/
+ *
+ * (c) PHP Telegram Bot Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Start command
+ *
+ * Gets executed when a user first starts using the bot.
+ *
+ * When using deep-linking, the parameter can be accessed by getting the command text.
+ *
+ * @see https://core.telegram.org/bots#deep-linking
+ */
+
+namespace Longman\TelegramBot\Commands\SystemCommands;
+
+use Longman\TelegramBot\Commands\SystemCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request;
+
+class StartCommand extends SystemCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'start';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Start command';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/start';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.2.0';
+
+    /**
+     * @var bool
+     */
+    protected $private_only = true;
+
+    /**
+     * Main command execution
+     *
+     * @return ServerResponse
+     * @throws TelegramException
+     */
+    public function execute(): ServerResponse
+    {
+
+        $message = $this->getMessage();            // Get Message object
+
+        $chat_id = $message->getChat()->getId();   // Get the current Chat ID
+
+        $data = [                                  // Set up the new message data
+            'chat_id' => $chat_id,                 // Set Chat ID to send the message to
+            'text'    => 'This is start command ...', // Set message to send
+        ];
+
+        return Request::sendMessage($data);        // Send message!
+
+    }
+}
