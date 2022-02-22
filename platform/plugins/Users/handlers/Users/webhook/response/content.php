4f58be4f215cd38611455bfb99c0960fc8800791
Pavan Yogi <pavanyogi121@gmail.com>
2022-02-22 13:14:02 +0530

    commented include of webhook.js

--- platform/plugins/Users/handlers/Users/webhook/response/content.php
+++ platform/plugins/Users/handlers/Users/webhook/response/content.php
@@ -2,6 +2,6 @@
 
 function Users_webhook_response_content($params)
 {
-    Q_Response::addScript("{{Users}}/js/pages/webhook.js");
+//    Q_Response::addScript("{{Users}}/js/pages/webhook.js");
     return Q::view("Users/content/webhook.php");
 }
