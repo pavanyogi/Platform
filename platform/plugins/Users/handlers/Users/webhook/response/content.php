9ddc4028b111e03f98510a30e437385de2a756fb
Pavan Yogi <pavanyogi121@gmail.com>
2022-02-23 21:42:50 +0530

    commented telegram data validate event

--- platform/plugins/Users/handlers/Users/webhook/response/content.php
+++ platform/plugins/Users/handlers/Users/webhook/response/content.php
@@ -4,7 +4,7 @@ function Users_webhook_response_content($params)
 {
 //    Q_Response::addScript("{{Users}}/js/pages/webhook.js");
 
-    $authData = Q::event('Users/webhook/validate', $params);
+//    $authData = Q::event('Users/webhook/validate', $params);
 
     return Q::view("Users/content/webhook.php");
 }
