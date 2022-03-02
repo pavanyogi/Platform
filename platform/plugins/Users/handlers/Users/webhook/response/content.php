51b8968c0ef4acf162b48a2cb3094cab911831fc
Pavan Yogi <pavanyogi121@gmail.com>
2022-03-02 19:20:21 +0530

    Add redirect to profile

--- platform/plugins/Users/handlers/Users/webhook/response/content.php
+++ platform/plugins/Users/handlers/Users/webhook/response/content.php
@@ -2,8 +2,10 @@
 
 function Users_webhook_response_content($params)
 {
-    $redirect = Q_Uri::url("TokenSociety/profile");
-    Q_Response::redirect($redirect);
-    return true;
+//    if (isset($_REQUEST['id']) and isset($_REQUEST['auth_date']) and isset($_REQUEST['hash']) ) {
+//        $redirect = Q_Uri::url("TokenSociety/profile") . http_build_query($params, null, '&');;
+//        Q_Response::redirect($redirect);
+//        return true;
+//    }
 
 }
