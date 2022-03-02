b37fc34fe5015da8aa8a226db9405d1bcce76e5a
Pavan Yogi <pavanyogi121@gmail.com>
2022-03-02 18:15:00 +0530

    Add redirect after successful telegram login

--- platform/plugins/Users/handlers/Users/webhook/response/content.php
+++ platform/plugins/Users/handlers/Users/webhook/response/content.php
@@ -2,20 +2,8 @@
 
 function Users_webhook_response_content($params)
 {
-//    Q_Response::addScript("{{Users}}/js/pages/webhook.js");
+    $redirect = Q_Uri::url("TokenSociety/profile");
+    Q_Response::redirect($redirect);
+    return true;
 
-//    $authData = Q::event('Users/webhook/validate', $params);
-
-    if (isset($_REQUEST['hash']) && isset($_REQUEST['username'])) {
-        $platform = 'telegram';
-        $appId = Q_Config::get('Users', 'apps', 'telegram', 'TokenSociety', 'appId', null);
-        $user = Users::authenticate($platform, $appId, $authenticated);
-        Q::log(array('$user' => $user), 'Users');
-        if (!$user) {
-            throw new Users_Exception_NotLoggedIn();
-        }
-        Users::setLoggedInUser($user);
-    }
-
-    return Q::view("Users/content/webhook.php");
 }
